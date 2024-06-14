54afdf99d725f8bde169bc24c2dcd9751adb5e3b
kimnari1073 <nari1073@naver.com>
2024-06-14 16:58:16 +0900

    create project

---
+++ member-blog/src/main/java/com/example/member_blog/MemberBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.member_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberBlogApplication.class, args);
+	}
+
+}
